ae5dec331f3819d99fa95e7630f1624d7b218312
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2024-01-31 19:01:56 -0300

    finalizando exercícios

--- atividades/exercicios_funcoes.js
+++ atividades/exercicios_funcoes.js
@@ -366,18 +366,87 @@ function lanchonete(quantidade, codigo){
 lanchonete(2, 100)
 
 //Exercício 20
-/* function imprimirCedulas(valor){
-    let notasUtilizadas = []
-    for(let quantidade = 0; nota100 = valor; quantidade ){
-        console.log(typeOf(nota100))
-        if(nota100 <= valor){
-            console.log(notasUtilizadas.push = nota100)
-      
+function sacarDinheiro(valorSaque) {
+    let contador100 = 0
+    let contador50 = 0
+    let contador10 = 0
+    let contador5 = 0
+    let contador1 = 0
+    let valorNota = calcularValorNota(valorSaque)
+    while (valorSaque >= valorNota) {
+        switch (valorNota) {
+            case 100:
+                valorSaque -= 100
+                contador100++
+                break
+            case 50:
+                valorSaque -= 50
+                contador50++
+                break
+            case 10:
+                valorSaque -= 10
+                contador10++
+                break
+            case 5:
+                valorSaque -= 5
+                contador5++
+                break
+            case 1:
+                contador1++
+                valorSaque -= 1
+                break
         }
+
+        valorNota = calcularValorNota(valorSaque)
+
+    }
+    return elaborarResultado(contador100, contador50, contador10, contador5, contador1)
+}
+
+function calcularValorNota(valorSaque) {
+    if (valorSaque >= 100) {
+        return 100
+    } else if (valorSaque >= 50) {
+        return 50
+    } else if (valorSaque >= 10) {
+        return 10
+    } else if (valorSaque >= 5) {
+        return 5
+    } else if (valorSaque >= 1) {
+        return 1
     }
 }
 
-console.log(imprimirCedulas(200)) */
+function elaborarResultado(contador100, contador50, contador10, contador5, contador1) {
+    let resultado = ''
+
+    if (contador100 > 0) {
+        resultado += `${contador100} nota(s) de R$ 100. `
+    }
+
+    if (contador50 > 0) {
+        resultado += `${contador50} nota(s) de R$ 50. `
+    }
+
+    if (contador10 > 0) {
+        resultado += `${contador10} nota(s) de R$ 10. `
+    }
+
+    if (contador5 > 0) {
+        resultado += `${contador5} nota(s) de R$ 5. `
+    }
+
+    if (contador1 > 0) {
+        resultado += `${contador1} nota(s) de R$ 1. `
+    }
+
+    return resultado
+}
+
+console.log(sacarDinheiro(153));
+
+
+console.log(imprimirCedulas(200))
 
 //Exercício 21
 function planoDeSaude(idade){
@@ -496,27 +565,45 @@ while(num1 <= 100){
 }
 
 //Exercício 27
-/* function crescimento(altura1, taxa1, altura2, taxa2){
-    let Altura1 = altura1 * 100
-    let Altura2 = altura2 * 100
-    let Taxa1 = taxa1 * 100
-    let Taxa2 = taxa2 * 100
-
-    if(Altura1 < Altura2){
-        
-        for(let anos = 0; Altura1 == Altura2; anos++){
-             Altura1 + Taxa1
-            console.log(anos)
+function calcularCrescimento(altura1, taxa1, altura2, taxa2) {
+    if(altura1 == altura2){
+        if (taxa1 > taxa2) {
+            return 'A criança 1 ultrapassará a criança 2 em 1 ano.'
+        } else if(taxa1 < taxa2) {
+            return 'A criança 2 ultrapassará a criança 1 em 1 ano.'
+        }else{
+            return 'As crianças tem igual altura e crescimento.'
         }
-        if(Altura2 < Altura1){
-            
+    } else {
+        if (altura1 > altura2) {
+            if(taxa1 >= taxa2){
+                return 'A criança menor não ultrapassará a maior.'
+            }else{
+               return `A criança menor ultrapassará a maior em ${calcularTempo(altura2, taxa2, altura1, taxa1)} anos` 
+            }
+        } else {
+            if(taxa2 >= taxa1){
+                return 'A criança menor não ultrapassará a maior.'
+            }else{
+               return `A criança menor ultrapassará a maior em ${calcularTempo(altura1, taxa1, altura2, taxa2)} anos` 
+            }
         }
-
     }
-
 }
 
-crescimento(1.80, 0.20, 1.90, 0.10) */
+function calcularTempo(alturaMenor, taxaAlturaMenor, alturaMaior, taxaAlturaMaior){
+    let qtdAnos = 0
+    while (alturaMenor < alturaMaior) {
+        alturaMenor += taxaAlturaMenor
+        alturaMaior += taxaAlturaMaior
+        qtdAnos++
+    }
+    //console.log(qtdAnos)
+    return qtdAnos
+}
+
+console.log(calcularCrescimento(150, 2, 130, 4));
+
 
 //Exercício 28
 function imprimirParesImpares(numero){
